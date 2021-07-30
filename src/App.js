ecec00ba8b2b49630b0600e0e5ad4f69df11252c
Sary Jamie <sary.jamie@gmail.com>
2021-07-29 17:58:01 -0700

    importing components into App.js and adding them to the Route components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import SinglePost from "./components/SinglePost";
+import Post from "./components/Post";
+import Project from "./components/Project";
 
 function App() {
   return (
-
+    <BrowserRouter>
+      <Switch>
+        <Route component={Home} path="/" exact />
+        <Route component={About} path="/about" />
+        <Route component={SinglePost} path="/post/:slug" />
+        <Route component={Post} path="/post" />
+        <Route component={Project} path="/project" />
+      </Switch>
+    </BrowserRouter>
   );
 }
 

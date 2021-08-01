45bd260a9ef73efa7dd28bd73d533b1514495a54
Sary Jamie <sary.jamie@gmail.com>
2021-07-31 23:18:45 -0700

    adding an image to the home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,7 +1,21 @@
 import React from "react";
+import image from "../background.jpg";
 
 const Home = () => {
-  return <h1>Home Page!</h1>;
+  return (
+    <main>
+      <img
+        src={image}
+        alt="Work Desk"
+        className="absolute object-conver w-full h-full"
+      />
+      <section className="relative flex justify-center min-h-screen pt-12 lg:pt-24 px-8">
+        <h6 className="text-20xl text-blue-300 font-bold cursive leading-none lg:leading-snug home-name">
+          HELLO..
+        </h6>
+      </section>
+    </main>
+  );
 };
 
 export default Home;

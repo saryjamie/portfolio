ad8a99418022fdfc4337eb349a9af90422411152
Sary Jamie <sary.jamie@gmail.com>
2021-08-04 03:16:43 -0700

    installing @sanity/image-url and setting up SinglePage.js

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import sanityClient from "../client.js";
+import imageUrlBuilder from "@sanity/image-url";
 
 const SinglePost = () => {
-  return <h1>SinglePost Page!</h1>;
+  const [singlePost, setSinglePost] = useState(null);
+  const { slug } = useParams();
 };
 
 export default SinglePost;

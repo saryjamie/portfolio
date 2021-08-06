e1cf6432a17a5acaf7d04c2534ff9f203b92d19a
Sary Jamie <sary.jamie@gmail.com>
2021-08-06 11:44:23 -0700

    adding clickable buttons to project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -33,14 +33,14 @@ const Project = () => {
             projectData.map((project, index) => (
               <article className="relative reounded-lg shadow-xl bg-white p-16">
                 <h3 className="text-gray-800 text-3xl font-bold mb-2 hover:text-red-700">
-                  {/* <a
+                  <a
                     href={project.link}
                     alt={project.title}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
                     {project.title}
-                  </a> */}
+                  </a>
                 </h3>
                 <div className="text-gray-500 text-xs space-x-4">
                   <span>
@@ -58,17 +58,17 @@ const Project = () => {
                   <p className="my-6 text-lg text-gray-700 leading-relaxed">
                     {project.description}
                   </p>
-                  {/* <a
+                  <a
                     href={project.link}
                     rel="noopener noreferrer"
                     target="_blank"
-                    className="text-red-700 font-bold text-xl hover:underline hover:text-red-400"
-                  > */}
-                  View The Project{" "}
-                  <span role="img" aria-label="right pointer">
-                    ➡️
-                  </span>
-                  {/* </a> */}
+                    className="text-red-500 font-bold text-xl hover:underline hover:text-red-400"
+                  >
+                    Live{" "}
+                    {/* <span role="img" aria-label="right pointer">
+                      ➡️
+                    </span> */}
+                  </a>
                 </div>
               </article>
             ))}

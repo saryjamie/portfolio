81fa415e362a88c013066f2e452a703ae81ab36e
saryjamie <sary.jamie@gmail.com>
2021-08-18 01:44:45 -0700

    fixing cards margins and bg color in Project.js

--- src/components/Project.js
+++ src/components/Project.js
@@ -39,65 +39,71 @@ const Project = () => {
         <section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
           {projectData &&
             projectData.map((project, index) => (
-              <article className="block h-128 relative rounded reounded-lg shadow-xl bg-white p-10 border">
-                <h3 className="text-gray-800 text-xl font-bold mb-2 hover:text-red-700">
-                  <a
-                    href={project.link}
-                    alt={project.title}
-                    target="_blank"
-                    rel="noopener noreferrer"
-                  >
-                    {project.title}
-                  </a>
-                </h3>
-                <div className="text-gray-500 text-xs space-x-4 pr-8">
-                  <span>
-                    <strong className="font-bold">Finished on</strong>:{" "}
-                    {new Date(project.date).toLocaleDateString()}
-                  </span>
-                  <span>
-                    <strong className="font-bold">Company</strong>:{" "}
-                    {project.place}
-                  </span>
-                  <span>
-                    <strong className="font-bold">Type</strong>:{" "}
-                    {project.projectType}
-                  </span>
-                  <p className="my-3 text-lg text-gray-600 leading-relaxed">
-                    {project.description}
-                  </p>
-                  <a
-                    href={project.link}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                  >
-                    <img
-                      src={project.mainImage.asset.url}
-                      alt={project.mainImage.asset.alt}
-                      className="relative shadow rounded mb-8"
-                      style={{ height: "250px", width: "450px" }}
-                    />
-                  </a>
-                  <a
-                    href={project.link}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="text-blue-700 font-bold text-xl hover:underline hover:text-red-500 justify-center"
-                  >
-                    LIVE{" "}
-                    {/* <span role="img" aria-label="right pointer">
+              <article>
+                <span className="block h-160 relative rounded reounded-lg shadow-xl bg-blue-200 p-5 border">
+                  <h3 className="text-gray-800 text-xl font-bold mb-2 hover:text-red-700">
+                    <a
+                      href={project.link}
+                      alt={project.title}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                    >
+                      {project.title}
+                    </a>
+                  </h3>
+                  <div className="text-gray-500 text-xs space-x-4 pr-8">
+                    <span>
+                      <strong className="font-bold">Finished on</strong>:{" "}
+                      {new Date(project.date).toLocaleDateString()}
+                    </span>
+                    <span>
+                      <strong className="font-bold">Company</strong>:{" "}
+                      {project.place}
+                    </span>
+                    <span>
+                      <strong className="font-bold">Type</strong>:{" "}
+                      {project.projectType}
+                      <p className="my-3 text-lg text-gray-600 leading-relaxed">
+                        {project.description}
+                      </p>
+                      <a
+                        href={project.link}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                      >
+                        <img
+                          src={project.mainImage.asset.url}
+                          alt={project.mainImage.asset.alt}
+                          className="relative shadow rounded mb-8"
+                          style={{
+                            marginBottom: "5",
+                            height: "250px",
+                            width: "750",
+                          }}
+                        />
+                      </a>
+                      <a
+                        href={project.link}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="text-blue-700 font-bold text-xl hover:underline hover:text-red-500 justify-center"
+                      >
+                        LIVE{" "}
+                        {/* <span role="img" aria-label="right pointer">
                       ➡️
                     </span> */}
-                  </a>
-                  <a
-                    href={project.githublink}
-                    rel="noopener noreferrer"
-                    target="_blank"
-                    className="text-blue-700 font-bold text-xl hover:underline hover:text-red-500 justify-center"
-                  >
-                    GitHub{" "}
-                  </a>
-                </div>
+                      </a>
+                      <a
+                        href={project.githublink}
+                        rel="noopener noreferrer"
+                        target="_blank"
+                        className="m-10 text-blue-700 font-bold text-xl hover:underline hover:text-red-500 justify-center"
+                      >
+                        GitHub{" "}
+                      </a>
+                    </span>
+                  </div>
+                </span>
               </article>
             ))}
         </section>

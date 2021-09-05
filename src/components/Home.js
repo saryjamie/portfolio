91fbd426362ec4db20eca85d6d9fa8c0bb2e7c7e
saryjamie <sary.jamie@gmail.com>
2021-09-05 06:49:39 -0700

    updating alt name in Home.js img tag

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,8 +6,8 @@ const Home = () => {
     <main>
       <img
         src={image}
-        alt="San Diego"
-        className="absolute object-cover w-full h-full"
+        alt="World Map Binary"
+        className="absolute opacity-10 object-cover w-full h-full"
       />
       <section className="relative flex justify-center min-h-screen pt-12 lg:pt-44 px-8">
         <h1 className="text-6xl text-gray-500 font-bold cursivea leading-none lg:leading-snug home-name">

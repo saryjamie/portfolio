82096b84fedbd36c2ffaaa7995bb3abd3e6ba3d3
saryjamie <sary.jamie@gmail.com>
2021-09-08 16:54:41 -0700

    updating the level of blue background to 100 in singlePost.js

--- src/components/SinglePost.js
+++ src/components/SinglePost.js
@@ -38,7 +38,7 @@ const SinglePost = () => {
   if (!singlePost) return <div>Loading...</div>;
 
   return (
-    <main className="bg-blue-200 min-h-screen p-12">
+    <main className="bg-blue-100 min-h-screen p-12">
       <article className="container shadow-lg mx-auto bg-geen-100 rounded-lg">
         <header className="relative">
           <div className="absolute h-full w-full flex items-center justify-center p-8">

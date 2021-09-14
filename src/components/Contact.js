47389f1c5ce4853a3e9b30b03bdc3c46de82b736
saryjamie <sary.jamie@gmail.com>
2021-09-14 06:29:46 -0700

    updating Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -2,9 +2,23 @@ import React from "react";
 
 const Contact = () => {
   return (
-    <div>
-      <label>Name</label>
-      <text></text>
+    <div class="form-container">
+      <form class="contact-form">
+        <h1>CONTACT</h1>
+        <input type="text" id="name" placeholder="Full Name" />
+        <br />
+        <input type="email" id="email" placeholder="Email" />
+        <br />
+        <input type="text" id="subject" placeholder="Subject" />
+        <br />
+        <textarea
+          id="message"
+          placeholder="Message"
+          cols="30"
+          rows="10"
+        ></textarea>
+        <br />
+      </form>
     </div>
   );
 };

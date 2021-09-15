15452fdc4d9ef2ee27d05e73c9a3a43b0e3d1fca
saryjamie <sary.jamie@gmail.com>
2021-09-15 10:28:44 -0700

    fixing margins in the contact form

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -20,26 +20,28 @@ const Contact = () => {
           <h1 className="text-5xl text-gray-700 flex justify-center cursive mb-10">
             Contact Me
           </h1>
-          <input type="text" id="name" placeholder="Full Name" />
-          <br />
-          <br />
-          <input type="email" id="email" placeholder="Email" />
-          <br />
-          <br />
-          <input type="text" id="subject" placeholder="Subject" />
-          <br />
-          <br />
-          <textarea
-            id="message"
-            placeholder="Message"
-            cols="30"
-            rows="10"
-          ></textarea>
-          <br />
-          <br />
-          <Button type="submit" value="Submit" variant="secondary">
-            Submit
-          </Button>
+          <div className="absolute mt-10 ml-10 w-80 h-600 ">
+            <input type="text" id="name" placeholder="Full Name" />
+            <br />
+            <br />
+            <input type="email" id="email" placeholder="Email" />
+            <br />
+            <br />
+            <input type="text" id="subject" placeholder="Subject" />
+            <br />
+            <br />
+            <textarea
+              id="message"
+              placeholder="Message"
+              cols="30"
+              rows="10"
+            ></textarea>
+            <br />
+            <br />
+            <Button type="submit" value="Submit" variant="secondary">
+              Submit
+            </Button>
+          </div>
         </form>
       </div>
     </main>

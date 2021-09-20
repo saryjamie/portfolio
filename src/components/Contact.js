c7bba05d0379ad934ad811c94d3ead08e2ae6682
saryjamie <sary.jamie@gmail.com>
2021-09-20 13:16:07 -0700

    passing a className to the conatct form div to use in the css file

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -21,7 +21,7 @@ const Contact = () => {
           <h1 className="text-5xl text-gray-700 flex justify-center cursive mb-10">
             Contact Me
           </h1>
-          <div className="absolute mt-2 ml-10 w-80 h-600 ">
+          <div className="formcss absolute mt-2 ml-10 w-80 h-600 ">
             <input type="text" id="name" placeholder="Full Name" />
             <br />
             <br />

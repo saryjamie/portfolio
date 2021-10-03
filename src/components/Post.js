e98fcb9d628f8aae5f164f91b0d3b6db05bc30c9
saryjamie <sary.jamie@gmail.com>
2021-10-03 09:43:59 -0700

    importing image into the Post.js file to set as a background

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import sanityClient from "../client.js";
+import image from "./codebg.jpg";
 
 const Post = () => {
   const [postData, setPost] = useState(null);

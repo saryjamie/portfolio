dab3c1e59a4b90803e6fdbfb65b008b1231536b3
saryjamie <sary.jamie@gmail.com>
2021-10-03 09:53:47 -0700

    adding a div before the section tag with padding and a class of relative to set the background nicely

--- src/components/Project.js
+++ src/components/Project.js
@@ -33,88 +33,90 @@ const Project = () => {
   if (!projectData) return <div className="loader">Loading...</div>;
 
   return (
-    <main>
+    <main className="relative">
       <img
         src={image}
         alt="World Map Binary"
         className="absolute opacity-20 w-full h-full"
       />
-      <section className="container mx-auto">
-        <h1 className="text-5xl text-gray-700 flex justify-center cursive">
-          Projects
-        </h1>
-        <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome To My Projects Page
-        </h2>
-        <section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
-          {projectData &&
-            projectData.map((project) => (
-              <article>
-                <span className="block h-160 relative rounded reounded-lg shadow-xl bg-blue-200 p-5 border">
-                  <h3 className="text-gray-800 text-xl font-bold mb-2 hover:text-red-600">
-                    <a
-                      href={project.link}
-                      alt={project.title}
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {project.title}
-                    </a>
-                  </h3>
-                  <div className="text-gray-500 text-xs space-x-4 pr-8">
-                    <span>
-                      <strong className="font-bold">Finished on</strong>:{" "}
-                      {new Date(project.date).toLocaleDateString()}
-                    </span>
-                    <span>
-                      <strong className="font-bold">Company</strong>:{" "}
-                      {project.place}
-                    </span>
-                    <span>
-                      <strong className="font-bold">Type</strong>:{" "}
-                      {project.projectType}
-                      <p className="my-3 text-lg text-gray-600 leading-relaxed">
-                        {project.description}
-                      </p>
+      <div className="p-10 lg:pt-12 container mx-auto relative">
+        <section className="container mx-auto">
+          <h1 className="text-5xl text-gray-700 flex justify-center cursive">
+            Projects
+          </h1>
+          <h2 className="text-lg text-gray-600 flex justify-center mb-12">
+            Welcome To My Projects Page
+          </h2>
+          <section className="grid grid-cols-1 lg:grid-cols-3 gap-8">
+            {projectData &&
+              projectData.map((project) => (
+                <article>
+                  <span className="block h-160 relative rounded reounded-lg shadow-xl bg-blue-200 p-5 border">
+                    <h3 className="text-gray-800 text-xl font-bold mb-2 hover:text-red-600">
                       <a
                         href={project.link}
-                        rel="noopener noreferrer"
+                        alt={project.title}
                         target="_blank"
-                      >
-                        <img
-                          src={project.mainImage.asset.url}
-                          alt={project.mainImage.asset.alt}
-                          className="relative shadow rounded mb-8"
-                          style={{
-                            marginBottom: "5",
-                            height: "250px",
-                            width: "750",
-                          }}
-                        />
-                      </a>
-                      <a
-                        href={project.link}
                         rel="noopener noreferrer"
-                        target="_blank"
-                        className="text-blue-700 font-bold text-xl hover:text-red-600 justify-center"
                       >
-                        LIVE{" "}
+                        {project.title}
                       </a>
-                      <a
-                        href={project.githublink}
-                        rel="noopener noreferrer"
-                        target="_blank"
-                        className="m-10 text-blue-700 font-bold text-xl hover:text-red-600 justify-center"
-                      >
-                        GitHub{" "}
-                      </a>
-                    </span>
-                  </div>
-                </span>
-              </article>
-            ))}
+                    </h3>
+                    <div className="text-gray-500 text-xs space-x-4 pr-8">
+                      <span>
+                        <strong className="font-bold">Finished on</strong>:{" "}
+                        {new Date(project.date).toLocaleDateString()}
+                      </span>
+                      <span>
+                        <strong className="font-bold">Company</strong>:{" "}
+                        {project.place}
+                      </span>
+                      <span>
+                        <strong className="font-bold">Type</strong>:{" "}
+                        {project.projectType}
+                        <p className="my-3 text-lg text-gray-600 leading-relaxed">
+                          {project.description}
+                        </p>
+                        <a
+                          href={project.link}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                        >
+                          <img
+                            src={project.mainImage.asset.url}
+                            alt={project.mainImage.asset.alt}
+                            className="relative shadow rounded mb-8"
+                            style={{
+                              marginBottom: "5",
+                              height: "250px",
+                              width: "750",
+                            }}
+                          />
+                        </a>
+                        <a
+                          href={project.link}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          className="text-blue-700 font-bold text-xl hover:text-red-600 justify-center"
+                        >
+                          LIVE{" "}
+                        </a>
+                        <a
+                          href={project.githublink}
+                          rel="noopener noreferrer"
+                          target="_blank"
+                          className="m-10 text-blue-700 font-bold text-xl hover:text-red-600 justify-center"
+                        >
+                          GitHub{" "}
+                        </a>
+                      </span>
+                    </div>
+                  </span>
+                </article>
+              ))}
+          </section>
         </section>
-      </section>
+      </div>
     </main>
   );
 };

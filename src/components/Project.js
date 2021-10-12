b53a6e84056099448808213be3c031aaa275f4d5
saryjamie <sary.jamie@gmail.com>
2021-10-12 16:15:24 -0700

    adding noreferrer props to the a tag for the rel

--- src/components/Project.js
+++ src/components/Project.js
@@ -57,7 +57,7 @@ const Project = () => {
                         href={project.link}
                         alt={project.title}
                         target="_blank"
-                        rel="noopener "
+                        rel="noopener noreferrer"
                         className="text-decoration-none"
                       >
                         {project.title}

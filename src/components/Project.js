21d7549ac50c38c0a9d282ead43c9ce7fbbfff16
saryjamie <sary.jamie@gmail.com>
2021-10-12 16:14:18 -0700

    minor change

--- src/components/Project.js
+++ src/components/Project.js
@@ -57,7 +57,7 @@ const Project = () => {
                         href={project.link}
                         alt={project.title}
                         target="_blank"
-                        rel="noopener noreferrer"
+                        rel="noopener "
                         className="text-decoration-none"
                       >
                         {project.title}

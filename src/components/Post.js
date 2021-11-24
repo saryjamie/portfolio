03ce8acc1432e11e90824cd9433437b148074dde
saryjamie <sary.jamie@gmail.com>
2021-11-24 03:15:37 -0800

    updating title in Post.js

--- src/components/Post.js
+++ src/components/Post.js
@@ -35,7 +35,7 @@ const Post = () => {
           Blogs
         </h1>
         <h2 className="text-lg text-gray-600 flex justify-center mb-12">
-          Welcome to my blogs page
+          Welcome to my blog page
         </h2>
         <div className="grid md:grid-cols-2 lg:grid-cols-3 gap-8">
           {postData &&
